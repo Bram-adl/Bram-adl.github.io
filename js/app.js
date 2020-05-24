492e5854d5724a87900d2b2a31c0178bc560184c
Bram-adl <Bintangrmdhn07@gmail.com>
2020-05-24 21:36:17 +0700

    Finish working for responsive home

--- js/app.js
+++ js/app.js
@@ -7,3 +7,18 @@
 		navbar.classList.toggle('clicked')
 	})
 })()
+
+/* Text Animation */
+function changeText() {
+	const TEXTS = ['Web Programmer', 'Web Designer', 'Web Developer']
+	const textEl = document.getElementById('text')
+
+	let index = 0
+	setInterval( () => {
+		index++
+		if ( index == TEXTS.length ) index = 0
+		const text = TEXTS[index]
+		textEl.innerHTML = text
+	}, 6000 )
+}
+changeText()
