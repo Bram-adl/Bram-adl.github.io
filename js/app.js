03ecdaaaf773efe5a07e19f84d7328a9db1d5c9a
Bram-adl <Bintangrmdhn07@gmail.com>
2020-05-25 00:15:42 +0700

    Create Portfolio Markup and Finish Working on Responsive Web

--- js/app.js
+++ js/app.js
@@ -8,8 +8,32 @@
 	})
 })()
 
+function setSection() {
+	const navbarMenu = document.querySelectorAll('.navbar__menu li')
+	let activeMenu = document.querySelector('.navbar__menu li.active')
+
+	navbarMenu.forEach(menu => {
+		menu.addEventListener('click', function() {
+			const currentSection = document.getElementById(activeMenu.dataset.section)
+			const targetSection = document.getElementById(this.dataset.section)
+			activeMenu.classList.remove('active')
+			this.classList.add('active')
+			activeMenu = this
+			moveSection(targetSection, currentSection)
+		})
+	})
+
+	function moveSection(target, current) {
+		target.classList.add('move-in')
+		target.classList.remove('move-out')
+		current.classList.add('move-out')
+		current.classList.remove('move-in')
+	}
+}
+setSection()
+
 /* Text Animation */
-function changeText() {
+function setChangeText() {
 	const TEXTS = ['Web Programmer', 'Web Designer', 'Web Developer']
 	const textEl = document.getElementById('text')
 
@@ -21,4 +45,43 @@ function changeText() {
 		textEl.innerHTML = text
 	}, 6000 )
 }
-changeText()
+setChangeText()
+
+/* Portfolio Filter */
+function setPortfolio() {
+	let activeTab = document.querySelector('.portfolio__tabs li.active')
+	const portfolioTabs = document.querySelectorAll('.portfolio__tabs li')
+	const portfolioImages = document.querySelectorAll('.portfolio__box')
+
+	portfolioTabs.forEach(tab => {
+		tab.addEventListener('click', (e) => {
+			activeTab.classList.remove('active')
+			e.target.classList.add('active')
+			activeTab = e.target
+			const dataset = e.target.dataset.type
+			setImages(dataset)
+		})
+	})
+
+	function setImages(dataset) {
+		if ( dataset == 'all' ) return showImage(portfolioImages)
+
+		let filtered = [...portfolioImages].filter(image => image.dataset.type == dataset)
+		let difference = [...portfolioImages].filter(image => !filtered.includes(image))
+		showImage(filtered)
+		hideImage(difference)
+	}
+
+	function showImage(images) {
+		images.forEach(image => {
+			image.style.display = 'block'
+		})
+	}
+
+	function hideImage(images) {
+		images.forEach(image => {
+			image.style.display = 'none'
+		})
+	}
+}
+setPortfolio()
